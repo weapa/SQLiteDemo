fffdc001d84fd60a9f77f8de0b7f5f5b28d6d513
piyada <waengsothon_p@silpakorn.edu>
2020-02-17 16:06:15 +0700

    final

--- app/src/main/java/th/ac/su/sqlitedemo/MainActivity.kt
+++ app/src/main/java/th/ac/su/sqlitedemo/MainActivity.kt
@@ -3,8 +3,11 @@ package th.ac.su.sqlitedemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_main.*
 import th.ac.su.sqlitedemo.adapter.ListStudentAdapter
+import th.ac.su.sqlitedemo.dbhelper.DBHelper
 import th.ac.su.sqlitedemo.model.Student
 
 class MainActivity : AppCompatActivity() {
@@ -13,39 +16,36 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var listViewStudent:ListView
 
+    lateinit var dbHelper: DBHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-        var student1 = Student(111,"piyada","piyada@gmail.com")
-        var student2 = Student(222,"waepa","waepa@gmail.com")
-        var student3 = Student(333,"ppra","ppra@gmail.com")
-        var student4 = Student(444,"ppra","ppra@gmail.com")
-        var student5 = Student(555,"ppra","ppra@gmail.com")
-        var student6 = Student(666,"ppra","ppra@gmail.com")
-        var student7 = Student(777,"ppra","ppra@gmail.com")
-        var student8 = Student(888,"ppra","ppra@gmail.com")
 
-        listStudent.add(student1)
-        listStudent.add(student2)
-        listStudent.add(student3)
-        listStudent.add(student4)
-        listStudent.add(student5)
-        listStudent.add(student6)
-        listStudent.add(student7)
-        listStudent.add(student8)
-
+        dbHelper = DBHelper(this)
 
         listViewStudent =  findViewById<ListView>(R.id.listStudent)
+        val btnAdd = findViewById<Button>(R.id.btnAdd)
+        val edtId = findViewById<EditText>(R.id.edtId)
+        val edtName = findViewById<EditText>(R.id.edName)
+        val edtEmail = findViewById<EditText>(R.id.edtEmail)
 
+        btnAdd.setOnClickListener {
+            val student= Student(edtId.text.toString().toInt(),edtName.text.toString(),edtEmail.text.toString())
+
+            dbHelper.addStudunt(student)
+            reloadData()
+
+        }
 
         reloadData()
     }
 
     fun reloadData(){
 
+        listStudent = dbHelper.allStudent
         val adapter = ListStudentAdapter(this@MainActivity,listStudent)
         listViewStudent.adapter = adapter
     }
--- app/src/main/java/th/ac/su/sqlitedemo/dbhelper/DBHelper.kt
+++ app/src/main/java/th/ac/su/sqlitedemo/dbhelper/DBHelper.kt
@@ -1,4 +1,64 @@
 package th.ac.su.sqlitedemo.dbhelper
 
-class DBHelper {
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import th.ac.su.sqlitedemo.model.Student
+
+class DBHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME,null,DATABASE_VERSION) {
+
+    companion object{
+        private val  DATABASE_VERSION = 1
+        private val  DATABASE_NAME = "univercity.db"
+        private val  TABIE_NAME = "student"
+        private val  COL_ID = "ID"
+        private val  COL_NAME = "name"
+        private val  COL_EMAIL = "email"
+    }
+    override fun onCreate(db: SQLiteDatabase?) {
+       val CREATE_TABLE = ("CREATE TABLE $TABIE_NAME ($COL_ID INTEGER PRIMARY KEY,$COL_NAME TEXT,$COL_EMAIL TEXT)")
+        db!!.execSQL(CREATE_TABLE)
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    val allStudent:ArrayList<Student>
+    get(){
+        val listStudent = ArrayList<Student>()
+
+        val SELECT_QUERY = "SELECT * FROM $TABIE_NAME"
+        val db: SQLiteDatabase = this.writableDatabase
+        val cursor: Cursor = db.rawQuery(SELECT_QUERY, null)
+
+        if (cursor.moveToFirst()){
+            do {
+                val student = Student()
+                student.id = cursor.getInt(cursor.getColumnIndex(COL_ID))
+                student.name = cursor.getString(cursor.getColumnIndex(COL_NAME))
+                student.email = cursor.getString(cursor.getColumnIndex(COL_EMAIL))
+
+                listStudent.add(student)
+
+            }while (cursor.moveToNext())
+        }
+
+        return  listStudent
+    }
+
+    fun addStudunt(student: Student){
+        val db:SQLiteDatabase = this.writableDatabase
+
+        val contentValue = ContentValues()
+        contentValue.put(COL_ID,student.id)
+        contentValue.put(COL_NAME,student.name)
+        contentValue.put(COL_EMAIL,student.email)
+
+        db.insert(TABIE_NAME,null,contentValue)
+        db.close()
+    }
+
 }
